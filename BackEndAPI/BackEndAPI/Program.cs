dc06e67de46e670bfafad80dd65253b34f7e03bf
Santiago Martinez <santiagodelgadillo980@gmail.com>
2025-10-05 11:24:35 -0500

    Declaracion Services, Controllers y uso de MapController en Program.cs

--- BackEndAPI/BackEndAPI/Program.cs
+++ BackEndAPI/BackEndAPI/Program.cs
@@ -20,18 +20,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaDeConnexion"))
 );
 
-builder.Services.AddAutoMapper(typeof(MappingProfile));
+#region Services
 
 builder.Services.AddScoped<IAdminComponetes, ComponenteServices>();
 builder.Services.AddScoped<IComponentes, ComponenteServices>();
 builder.Services.AddScoped<IConsultaComponentes, ComponenteServices>();
-
-
 builder.Services.AddScoped<IComponentesCompleto, ComponenteServices>();
 
 builder.Services.AddScoped<IEnsamblaje, EnsamblajeServices>();
-
 builder.Services.AddScoped<IUsuario, UsuarioServices>();
+
+#endregion
+
+
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -42,29 +45,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
